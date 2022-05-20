bd04304baa4b598fa0fae5f38de2d477b3e17cc0
Maria Lupu <maria.lupu@student.tuiasi.ro>
2022-05-20 15:38:59 +0300

    am incercat sa fac rezultatul la chestionar - ma bate

--- app.js
+++ app.js
@@ -23,22 +23,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.send('Hello World'));
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
+let listaIntrebari;
+const fs=require('fs');
 app.get('/chestionar', (req, res) => {
-	const listaIntrebari = [
-		{
-			intrebare: 'Who took the official photographs of the wedding of Prince Charles and Lady Diana in 1981?',
-			variante: ['Horst P. Horst', 'Patrick Lichfield','Frederick Sommer'],
-			corect: 1
-		},
-		//...
-	];
+	
+
+	fs.readFile('intrebari.json','utf8',(err,data)=>{
+
+		if(err){
+			console.error(err);
+			return;
+		}
+		listaIntrebari=JSON.parse(data);
+		res.render('chestionar', {intrebari: listaIntrebari});
+	});
 	// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
-	res.render('chestionar', {intrebari: listaIntrebari});
+});
+
+app.get('/rezultat-chestionar',(req,res) => {
+	res.redirect('/chestionar');
 });
 
 app.post('/rezultat-chestionar', (req, res) => {
-	console.log(req.body);
-	res.send("formular: " + JSON.stringify(req.body));
+	 var score=0;
+	 for(var i=0; i<intrebari.length; i++){
+		//var intrebare = intrebari[i];
+		var userAnswer = readlineSync.intrebare(intrebari[i].intrebare);
+		if(userAnswer == corect){
+			console.log("correct! ");
+    		score++;
+		}
+		else{
+			console.log("wrong! ");
+			score--;
+		  }
+	  }
+	// console.log(req.body);
+	// res.send("formular: " + JSON.stringify(req.body));
+	console.log("YOUR FINAL SCORE IS: " + score+"/10")
+	res.render('rezultat-chestionar',{listaIntrebari: score});
 });
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
